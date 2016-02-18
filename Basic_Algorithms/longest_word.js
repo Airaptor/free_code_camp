c7b7e262d682eb306b311ff4469dd38d7caad105
Thales Ferreira <thales.ferreira@ryerson.ca>
2016-02-18 00:13:04 -0500

    Update longest_word.js

--- Basic_Algorithms/longest_word.js
+++ Basic_Algorithms/longest_word.js
@@ -1,16 +1,21 @@
 function findLongestWord(str) {
   
 size = str.split(" ").length;
-  
-var length1 = 0, j=0;
-for (var i = 0; i<=size; i++  )
-{length = text.split(" ")[i].length;
-  if (length1 <= length){
-    length1 = length;
-    j=i;      }}
-  str = str[j];
-  return str.length;
-}
-//test
-findLongestWord("The quick brown fox jumped over the lazy dog");
 
+var temp = 0, i=0, biggest = 0;
+for (i = 0; i < size; i++  )
+{
+  temp = (str.split(" ")[i]).length;
+  if (biggest < temp)
+  {
+    biggest = temp;
+  }
+}
+  return biggest;
+}
+
+//testing
+findLongestWord("The quick brown fox jumped over the lazy dog");
+findLongestWord("Google do a barrel roll");
+findLongestWord("What is the average airspeed velocity of an unladen swallow");
+findLongestWord("What if we try a super-long word such as otorhinolaryngology");
